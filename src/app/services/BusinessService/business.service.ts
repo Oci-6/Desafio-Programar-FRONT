fb5b42ade39b1e5f14b3c2d86fe933529b9dbe96
ShadowCoffin <ShadowCoffin@users.noreply.github.com>
2021-07-05 15:39:09 -0300

    CRUD Empresa/Persona

--- src/app/services/BusinessService/business.service.ts
+++ src/app/services/BusinessService/business.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Business } from 'src/app/models/Business';
+import { Business_Person } from 'src/app/models/Business_Person';
 
 @Injectable({
   providedIn: 'root'
@@ -31,16 +32,21 @@ export class BusinessService {
     return this.http.delete(this.URL + '/' + id);
   }
 
-  // addPerson(){
-  //   return this.http.post(this.URL + '/addPerson' + );
-  // }
+  getBP(id: number) {
+    return this.http.get<Business_Person[]>(this.URL + '/persons/' + id)
+  }
 
-  // putBP(){
-  //   return this.http.put(this.URL + '/putBP' + );
-  // }
+  addPerson(BP: Business_Person) {
+    return this.http.post(this.URL + '/addPerson', BP);
+  }
 
-  // deleteBP(id: number){
-  //   return this.http.delete(this.URL + '/deleteBP' + id);
-  // }
+  putBP(BP: Business_Person) {
+    return this.http.put(this.URL + '/putBP/' + BP.id, {tipo: BP.tipo});
+  }
+
+  deleteBP(id: number) {
+    return this.http.delete(this.URL + '/deleteBP/' + id);
+  }
+
 }
 
